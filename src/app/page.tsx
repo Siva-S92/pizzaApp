28decccd2012458086ac1856e0d607457f24ae2b
sivausegithub <sivagraphics4fashion@gmail.com>
2024-04-13 16:58:56 +0530

    add shadcn ui theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
       <h1>welcome to the Pizza Shop</h1>
-      <Button variant="outline">Button</Button>
+      <Button>Button</Button>
     </main>
   );
 }
